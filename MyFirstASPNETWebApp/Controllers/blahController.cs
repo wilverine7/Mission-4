3a2ea0d2d58114cee21a4064a017f4cfc7e63d68
William Clayton <wclayto4@byu.edu>
2023-02-06 11:05:50 -0700

    Mission 5 changes applied

---
+++ MyFirstASPNETWebApp/Controllers/blahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstASPNETWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Controllers
+{
+    public class blahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstASPNETWebApp/Models/GradeCalculatorModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100)]
+        public int Assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int GroupProjects { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Quizzes { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Midterm { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Final { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int Intex { get; set; }
+    }
+}
